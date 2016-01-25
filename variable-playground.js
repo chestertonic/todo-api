60294a561000bc63760e6c7940d62825badc67ae
Chester <chestertonic@gmail.com>
2016-01-25 01:23:42 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+/*var person = {
+  name: "Chester",
+  age: 26
+};
+
+function updatePerson (obj, age) {
+  obj.age = age;
+}
+
+updatePerson(person, 29);
+console.log(person);
+*/
+var grades = [15, 37];
+
+function addGrade (arr, grade) {
+  arr.push(grade);
+  debugger;
+}
+
+addGrade(grades, 22);
+
+console.log(grades);
