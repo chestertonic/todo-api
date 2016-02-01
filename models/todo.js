5cbcd257f9f0ce0c58934d20b5908abca99cffc9
Chester <chestertonic@gmail.com>
2016-01-31 23:28:21 -0500

    Add login route and authenticate class method

--- models/todo.js
+++ models/todo.js
@@ -5,8 +5,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       validate: {
         len: [1, 250]
-      }
-    },
+      }},
     completed: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('user', {
+  var user = sequelize.define('user', {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -8,11 +11,26 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       }
     },
+    salt: {
+      type: DataTypes.STRING
+
+    },
+    password_hash: {
+      type: DataTypes.STRING
+    },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.VIRTUAL,
       allowNull: false,
       validate: {
         len: [7, 100]
+      },
+      set: function(value) {
+        var salt = bcrypt.genSaltSync(10);
+        var hashedPassword = bcrypt.hashSync(value, salt);
+
+        this.setDataValue('password', value);
+        this.setDataValue('salt', salt);
+        this.setDataValue('password_hash', hashedPassword);
       }
     }
   }, {
@@ -22,6 +40,37 @@ module.exports = function(sequelize, DataTypes) {
           user.email = user.email.toLowerCase();
         }
       }
+    },
+    classMethods: {
+      authenticate: function(body) {
+        return new Promise(function(resolve, reject) {
+          if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+            return reject(); //res.status(400).send();
+          }
+
+          user.findOne({
+            where: {
+              email: body.email
+            }
+          }).then(function(user) {
+            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+              return reject(); //res.status(401).send();
+            }
+
+            resolve(user); //res.json(user.toPublicJSON());
+          }, function(e) {
+            reject(); //res.status(500).send();
+          });
+        });
+      }
+    },
+    instanceMethods: {
+      toPublicJSON: function() {
+        var json = this.toJSON();
+        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+      }
     }
   });
+
+  return user;
 };
